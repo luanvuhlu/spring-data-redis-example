41d37bfb90dd9f4b9df6689a38ee4bceb7fd92d3
luan vu <luanvudev@gmail.com>
2023-09-14 00:08:52 +0700

    fix

--- src/main/java/com/luanvv/springdata/redis/example/AppConfig.java
+++ src/main/java/com/luanvv/springdata/redis/example/AppConfig.java
@@ -1,7 +1,6 @@
 package com.luanvv.springdata.redis.example;
 
 import java.util.List;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +8,6 @@ import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 
 /**
  * Application context configuration setting up {@link RedisConnectionFactory} and {@link RedisTemplate} according to
@@ -21,10 +19,10 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 @Configuration
 public class AppConfig {
 
+  List<String> clusterNodes = List.of("node1-redis-dev.com:6379");
+
   @Bean
-  RedisConnectionFactory connectionFactory(
-      @Value("${spring.redis.cluster.nodes}") List<String> nodes
-  ) {
-    return new LettuceConnectionFactory(new RedisClusterConfiguration(nodes));
+  RedisConnectionFactory connectionFactory() {
+    return new LettuceConnectionFactory(new RedisClusterConfiguration(clusterNodes));
   }
 }
